7ead4903070521176fb89b8f0d007eb45ed8de1b
125054516-ai <125054516@upq.edu.mx>
2026-09-17 19:05:58 -0600

    ejercicio commit xhamp

---
+++ index.php
@@ -0,0 +1,27 @@
+<!--<?php
+    echo "Hola mundo!";
+
+?>  -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        h1{
+            color: #00e5ff;
+        }
+    </style>
+</head>
+<body>
+   
+<h1>HOLA CRAYOLA</h1>
+
+  <h1>hola crayolaaaa</h1>
+  <h1>hola crayolaaaa</h1>
+
+  <h1>hola crayolaaaa</h1>
+  <h1>hola crayolaaaa</h1>
+</body>
+</html>
